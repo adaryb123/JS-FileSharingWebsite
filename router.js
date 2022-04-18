8d2a617976312dfa422e2ed3501750e5a7e1599a
David <david.pukanec@inclor.eu>
2022-04-18 19:39:49 +0200

    working download

--- router.js
+++ router.js
@@ -3,10 +3,112 @@ var path = require("path");
 var multer = require("multer");
 var crypto = require("crypto");
 
-var File = require("./model/fileSchema");
+var fs = require('fs');
+var mime = require('mime');
 
+var hostUrl = "http://localhost:3000/";
+
+// File scheme
+var File = require("./model/fileSchema");
+const { runInNewContext } = require("vm");
+const req = require("express/lib/request");
+
+// Get express router
 var router = express.Router();
 
+// Init file storage
+var file_storage = multer.diskStorage({
+    destination:"file_storage",
+    filename: function (req, file, cb) {
+        cb(null, req.uniqName + "--" + file.originalname);
+        req.storedFileName = req.uniqName + "--" + file.originalname;
+    }
+    
+});
+
+var upload = multer({storage:file_storage});
+
+
+router.get("/",function(req,res){
+    res.render("main_screen");
+});
+
+
+function insertFile(req, res, next){
+    uniquestr = crypto.pseudoRandomBytes(16).toString("hex");
+    console.log(uniquestr)
+    req.uniqName = uniquestr;
+    next();
+}
+
+// Upload file handler. Listen for upload post.
+router.post("/upload", insertFile, upload.single("inputFile"), (req, res) => {
+    try {
+        console.log('Started database saving');
+        var uniqueName = req.storedFileName;
+
+        console.log("Stored file: " + uniqueName);
+        var downloadURL = hostUrl + "download/" + crypto.pseudoRandomBytes(16).toString("hex");
+        var manageURL = hostUrl + "manage/" + crypto.pseudoRandomBytes(16).toString("hex");
+        var originalName = req.file.originalname;
+
+        var newFile = new File({
+            fileName: uniqueName,
+            downloadURL: downloadURL,
+            manageURL: manageURL,
+            originalName: originalName
+        });
+    
+        newFile.save();
+        console.log('Finished database saving');
+        urls = {down: downloadURL,
+                mana: manageURL }
+        res.send(urls);
+
+    } catch (err) {
+        console.log(err);
+    }
+
+    
+})
+
+
+router.post("/getfile", (req, res) => {
+    var fileKey = "req.fileLink;"
+    console.log(req.body);
+    File.findOne({downloadURL: hostUrl + "download/" + fileKey}, (err,data) =>{
+        if(err)
+            console.log(err);
+        else
+            console.log(data);
+            var absPath = __dirname+'\\file_storage\\' + data.fileName;
+
+            res.download(absPath,data.originalName);
+    })
+});
+
+router.get("/getfile/:fileKey", async (req, res) => {
+    console.log("Get");
+    var fileKey = req.params.fileKey;
+    console.log(hostUrl + "download/" + fileKey);
+    var data = await File.findOne({downloadURL: hostUrl + "download/" + fileKey})
+    console.log(data);
+    var absPath = __dirname+'\\file_storage\\' + data.fileName;
+
+    res.download(absPath,data.originalName);
+});
+
+
+router.get("/download/:fileKey", (req, res) => {
+    var fileKey = req.params.fileKey;
+    res.render("download_screen",{fileLink: fileKey})
+
+});
+
+
+
+
+/*
 var file_storage = multer.diskStorage({
     destination:"file_storage",
     filename: function (req, file, cb) {
@@ -16,11 +118,10 @@ var file_storage = multer.diskStorage({
  
 var upload = multer({storage:file_storage});
 
-router.get("/",function(req,res){
-    res.render("main_screen");
-});
 
+// Uploading
 router.post("/upload", upload.single("shared_file") ,function(req,res,next){
+    
     var filename = req.file.filename;
     var downloadURL = "download/" + crypto.pseudoRandomBytes(20).toString("hex")
     var manageURL = "manage/" + crypto.pseudoRandomBytes(20).toString("hex")
@@ -42,13 +143,10 @@ router.post("/upload", upload.single("shared_file") ,function(req,res,next){
     newFile.save(next);
 });
 
-router.get("/download",function(req,res){
-    res.render("download_screen")
-});
 
 router.get("/manage",function(req,res){
     res.render("manage_screen")
-});
+});*/
 
 
 module.exports = router;
--- static/main_screen.js
+++ static/main_screen.js
@@ -1,4 +1,49 @@
+
+const fileInput = document.getElementById("fileInput");
+const uploadBtn = document.getElementById("upload");
+const manageL = document.getElementById("manageL");
+const downloadL = document.getElementById("downloadL");
+const formUpload = document.getElementById("uploadForm")
+
+uploadBtn.addEventListener("click", function () {
+    const uploadForm = new FormData(formUpload);
+   
+    // For multiple files
+    /*for (const file of fileInput.files) {
+        console.log(file);
+        uploadForm.append("uploadFiles[]", file);
+    }*/
+
+    // For single file
+    uploadForm.append("inputFile", fileInput.files[0])
+
+    body = {
+        method: "post",
+        body: uploadForm
+    };
+    
+    console.log(body);
+    fetch("http://localhost:3000/upload",body)
+    .then( res => {
+        if (res.ok) {
+            console.log("Ok");
+        } else {
+            console.log("Fetch Failed");
+        }
+        return res.json();
+    })
+    .then(data => {
+        console.log(data);
+        downloadL.value = data.down;
+        manageL.value = data.mana;
+    })
+    .catch(error => console.error(error));
+
+})
+
+/*
 function uploadFile(){
     document.getElementById("manage-url").value = "test1";
     document.getElementById("download-url").value = "test2";
 }
+*/
