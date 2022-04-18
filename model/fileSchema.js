94b8ccc81f2ad7f77fcc0345b436cb2705bab902
David <david.pukanec@inclor.eu>
2022-04-18 19:39:49 +0200

    working download

--- model/fileSchema.js
+++ model/fileSchema.js
@@ -1,11 +1,12 @@
 var mongoose = require("mongoose");
 
 var fileSchema = mongoose.Schema({
-    filename:{type:String, required:true, unique:true},
+    fileName: {type:String, required:false, unique:true},
+    originalName:{type:String, required:false},
     downloadURL:{type:String, required:true, unique:true},
     manageURL:{type:String, required:true, unique:true},
     uploadDate:{type:Date,default:Date.now},
-    originalName:{type:String, required:true}
+   
 });
 
 var File = mongoose.model("File", fileSchema);
--- server.js
+++ server.js
@@ -9,15 +9,18 @@ mongoose.connect(database.URL);
   
 server.set("port", process.env.PORT || 3000);
 server.set("views", path.join(__dirname,"view"));
+
 // server.set("view engine", "ejs")
+
 server.engine('html', require('ejs').renderFile);
 server.set('view engine', 'html');
-
+server.set(express.urlencoded({extended: true}));
 server.use("/file_storage", express.static(path.resolve(__dirname,"file_storage")));
+
 
 server.use(bodyParser.urlencoded({extended:false}));
 // server.use(bodyParser.urlencoded({ extended:true, uploadDir: __dirname + "/file_storage" }));
-// server.use(bodyParser.json());
+//server.use(bodyParser.json());
 
 server.use("/static", express.static(path.resolve(__dirname,"static")));
 
